6f639b4f73f735cf49b8438a0b02107ac34044f1
aubrey <adaniels@axio.com>
2019-08-23 23:30:35 -0400

    Migration

--- timbr/backend/models.py
+++ timbr/backend/models.py
@@ -6,7 +6,7 @@ class CoordinatesRequested(models.Model):
     longitude = models.DecimalField(max_digits=17, decimal_places=14)
     request_time = models.DateTimeField(auto_now_add=True)
     ibm_classification = models.CharField(max_length=100, blank=True)
-    user_agrees_with_classification = models.BooleanField(blank=True)
+    user_agrees_with_classification = models.BooleanField(blank=True, null=True)
 
     def __str__(self):
         return self.request_time
